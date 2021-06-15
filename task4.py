931560d3468aad573dfe19c2a9137ea37d7cef8d
Gowtham-P-M <85796293+Gowtham-P-M@users.noreply.github.com>
2021-06-15 14:13:29 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,16 @@
+a=b=c=1
+print(a/10)
+print(b*50)
+print(c+60)
+print('\n')
+g="abcde"
+k=g[:2]+'G'+g[3:]
+print(k)
+print('\n')
+a=1
+b=1.0
+print(type(a),type(b))
+a=1.0
+b=1
+print(type(a),type(b))
+

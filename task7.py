f51d749edabb7665e72cc814227c8c69db0664da
Gowtham-P-M <85796293+Gowtham-P-M@users.noreply.github.com>
2021-06-18 21:05:56 +0530

    Add files via upload

---
+++ task7.py
@@ -0,0 +1,10 @@
+#1.Create a function getting two integer inputs from user
+def value(a,b):
+    print("Addition of two numbers is ",a+b,"\nSubtraction of two numbers is ",a-b,"\nMultiplication of two numbers is ",a*b,"\nDivision of two numbers is ",a/b)
+
+#2.Create a function covid( ) & it should accept patient name, and body temperature, by default the body temperature should be 98 degree
+def covid(p,bt=98):
+    print("Patient name "+p,"\nBody temperature",bt)
+
+
+
